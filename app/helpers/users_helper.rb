c9aa2fb99b69c3bd3ad573a6b553f46e80d5af51
井本翔太 <shota-imoto@imotoshoutanoMacBook-Air-2.local>
2021-09-12 15:11:11 +0900

    不要なコードを削除

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,8 +1,4 @@
 module UsersHelper
-  # def ajax_redirect_to(redirect_uri)
-  #   { js: "window.location.replace('#{redirect_uri}');" }
-  # end
-
   def get_recaptcha_response(token)
     siteverify_uri = URI.parse("https://www.google.com/recaptcha/api/siteverify?response=#{token}&secret=#{Rails.application.credentials.recaptcha[:recaptcha_secret_key]}")
     response = Net::HTTP.get_response(siteverify_uri)
